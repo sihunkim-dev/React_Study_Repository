0d7375cdccfdee4c1334d2a981e474c1256284f1
hoonie-dvp <sihun457@gmail.com>
2026-01-23 15:51:29 +0000

    Creat: To do List Architecture

---
+++ todolistdesign_front/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    const message = "Hello, World!";
+  return (
+    <div className="App">
+      <p>{message}</p>
+    </div>  
+  );
+}
+
+export default App;
