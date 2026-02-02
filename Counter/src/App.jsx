536e1b827d6eaefef1bccf710b6030ad0a4cd887
hoonie-dvp <sihun457@gmail.com>
2026-02-02 13:19:41 +0000

    Components : App, Header

--- Counter/src/App.jsx
+++ Counter/src/App.jsx
@@ -1,8 +1,43 @@
+import { useState } from "react";
 
+import Header from "./components/Header";
+
+////////////////////////////////////////////////////////////////////////
+/**
+ * Counter Practice
+ * Requirements : State Usage
+ * Components
+ *  - App
+ * - 
+ */
+////////////////////////////////////////////////////////////////////////
 function App(){
+  const[count, setCount] = useState(0);
+
+
+  function handleCount(type){
+    
+    setCount(prevCount => (type === 'plus'? prevCount + 1:prevCount -1));
+  }
+
   return(
     <>
-      <h1>Helloworld</h1>
+      <Header/> 
+      <div className="text-center bg-gray-700 items-center justify-center">
+            {count}
+      </div>
+      <div className="w-full mb-4 flex flex-nowrap justify-center gap-2">
+                <button className="bg-blue-300 active:bg-blue-500 px-4 py-2 rounded transition-all"
+                        onClick={()=>handleCount('plus')}
+                >
+                    PLUS
+                </button>
+                <button className="bg-red-300 active:bg-red-500 px-4 py-2 rounded transition-all"
+                        onClick={()=>handleCount('minus')}
+                >
+                    MINUS
+                </button>
+            </div>
     </>
   );
 }
---
+++ Counter/src/components/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header(){
+    return(
+        <div>
+            <h1 className="relative flex flex-col items-center justify-center py-10">Counter</h1>
+            <p className="text-5xl font-extrabold text-indigo-600 tracking-tight mb-2">Plus or Minus</p>
+        </div>
+    );
+}   
+//////////////////////// Documentation ////////////////////////////
+/**
+ * 1. Circle on Header 
+ * 2. Display the Main text on Header
+ */
