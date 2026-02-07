4cda2e9c9c0f4be9b931835a6bdad51e7a269421
hoonie-dvp <sihun457@gmail.com>
2026-02-07 01:35:26 +0000

    Add App.jsx

--- TodoMaker/src/App.jsx
+++ TodoMaker/src/App.jsx
@@ -1,14 +1,26 @@
-
+import { useState } from "react"
 
 function App() {
+  const[task, setTask] = useState("");
+  
+
+  function handleTaskInput(){
+    
+  }
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-white">
       <div className="flex flex-col bg-gray-50 border border-gray-200 rounded-xl shadow-md p-6 w-96 gap-4">
 
         <div className="flex gap-2">
-          <input type="text" placeholder="Enter daily task" className= "flex-1 px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-900 transition"/>
-          <button className="w-full bg-gray-800 text-white py-3 rounded-xl font-semibold hover:bg-gray-700 active:scale-[0.98] transition-all duration-200">
+          <input type="text" 
+                placeholder="Enter daily task" 
+                className= "flex-1 px-4 py-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-900 transition"/>
+          <button className="w-full bg-gray-800 text-white py-3 
+                            rounded-xl font-semibold hover:bg-gray-700 
+                            active:scale-[0.98] transition-all duration-200"
+
+          >
               ADD
           </button>        
         </div>
